5b43e9375ebe7b06ec905f12d348b178399bf98c
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2022-06-09 23:00:31 +0300

    Add samples

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Views/ExpanderViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Views/ExpanderViewModel.cs
@@ -1,9 +1,21 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace CommunityToolkit.Maui.Sample.ViewModels.Views;
 
 public partial class ExpanderViewModel : BaseViewModel
 {
 	[ObservableProperty]
-	bool isExpanded = true;
+	ObservableCollection<ContentCreator> contentCreators = new();
+
+	public ExpanderViewModel()
+	{
+		contentCreators.Add(new ContentCreator("Brandon Minnick", "https://codetraveler.io/"));
+		contentCreators.Add(new ContentCreator("Gerald Versluis", "https://blog.verslu.is/"));
+		contentCreators.Add(new ContentCreator("Pedro Jesus", "https://github.com/bijington"));
+		contentCreators.Add(new ContentCreator("Shaun Lawrence", "https://github.com/bijington"));
+		contentCreators.Add(new ContentCreator("Vladislav Antonyuk", "https://vladislavantonyuk.azurewebsites.net"));
+	}
 }
+
+public record ContentCreator(string Name, string Resource);
